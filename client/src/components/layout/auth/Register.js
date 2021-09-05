b72b14753832b77c74de0d7260f238c2ae27fd3b
rupalidashore <rupali.dashore@gmail.com>
2021-09-04 21:40:08 -0700

    Pix Chat App welcome message

--- client/src/components/layout/auth/Register.js
+++ client/src/components/layout/auth/Register.js
@@ -68,7 +68,7 @@ class Register extends Component {
             <div className="row">
               <div className="col-md-8 m-auto">
                 <h1 className="display-4 text-center">Sign Up</h1>
-                <p className="lead text-center">Create your PixchatApp account</p>
+                <p className="lead text-center">Create your PixchatApp Account</p>
                 <form noValidate onSubmit={this.onSubmit.bind(this)}>
                   <div className="form-group">
                     <input type="text" 
